61d22ce42a0ba42bb915ff25f0cbbd73a7c746fd
rajesh <rajeshkumarvuppula@gmail.com>
2016-12-21 11:11:29 +0530

    added a project

---
+++ retail/src/main/java/in/com/retail/App.java
@@ -0,0 +1,13 @@
+package in.com.retail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
